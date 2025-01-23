3ed965fdfcdfa98ed646bdc79360b8ebb76ad4c4
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2025-01-23 13:36:04 +0700

    chore: setup project

---
+++ src/views/DetailView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-green-500 text-center">
+      Detail
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailView'
+};
+</script>
